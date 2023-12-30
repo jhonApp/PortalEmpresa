3bd5c93fa87209f6f0f31d27c209ad5eb8bdb5d9
Jhon Everton <jhonevertonapp@outlook.com>
2023-12-30 12:03:41 -0300

    Criação do header e sidebar(em andamento)

--- site/src/components/Header/index.jsx
+++ site/src/components/Header/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import './Header.css';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const Header = () => {
-  console.log('estou no header')
   return (
-    <>
-      <header>
-        <span>Olá, fulano - WEST TOWERS</span>
-      </header>
-    </>
+    <AppBar position="static" sx={{ background: '#242C48', height: '100px' }}>
+      <Toolbar>
+        <Typography variant="h6" sx={{ fontFamily: 'Inter, sans-serif', color: '#fff', fontSize: '17px', textAlign: 'left', marginLeft: 'auto', marginTop: '40px' }}>
+          Olá, fulano - WEST TOWERS
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
