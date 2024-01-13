fcf1da9fe05353d8372d898e25e8e7fbea315470
Jhon Everton <jhonevertonapp@outlook.com>
2024-01-13 09:16:24 -0300

    Criação da tela de Login

---
+++ site/src/components/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/Header/Index';
+import Sidebar from './components/Sidebar';
+import GlobalStyle from './globalStyles';
+
+function Dashboard() {
+  const headerHeight = '64px';
+
+  return (
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: '40vh auto', // Definindo a largura do Sidebar como 40vh e o Header ocupando o restante do espaço
+      gap: '0', // Espaçamento entre os elementos
+      height: '100vh', // Definindo a altura total da página
+    }}>
+      <GlobalStyle />
+      <Sidebar headerHeight={headerHeight} style={{ height: '100vh' }} />
+      <Header />
+    </div>
+  );
+}
+
+export default Dashboard;
