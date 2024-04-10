ef88c845e410d3989488f63d548ac64b26f43723
Jhon Everton <jhonevertonapp@outlook.com>
2024-04-09 22:48:51 -0300

    Exibir anexos como modal carrosel

--- site/src/components/Dashboard/index.jsx
+++ site/src/components/Dashboard/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { getNaoVisualizados } from '../../../service/muralService';
-import { Box, Paper, Typography} from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 import Popup from '../Mural/Popup';
 import './Dashboard.css';
 import DadosDash from './DadosDash';
-import Table from '../Table'
-import {obeterUltimosAgendamentos} from '../../../service/agendamentoService';
+import Table from '../Table';
+import { obeterUltimosAgendamentos } from '../../../service/agendamentoService';
 
 function Dashboard() {
   const [openPopups, setOpenPopups] = useState([]);
@@ -16,10 +16,10 @@ function Dashboard() {
 
   const columns = [
     { id: 'nome', label: 'Nome', width: 100, align: 'center' },
-    { id: 'rgCpf', label: 'RG', minWidth: 80, align: 'center'},
-    { id: 'descricaoDataInicial', label: 'Data Inicial', minWidth: 140, align: 'center', },
-    { id: 'descricaoDataFim', label: 'Data Final', minWidth: 140, align: 'center', },
-    { id: 'status', label: 'Status', width: 150, align: 'center', }
+    { id: 'rgCpf', label: 'RG', minWidth: 80, align: 'center' },
+    { id: 'descricaoDataInicial', label: 'Data Inicial', minWidth: 140, align: 'center' },
+    { id: 'descricaoDataFim', label: 'Data Final', minWidth: 140, align: 'center' },
+    { id: 'status', label: 'Status', width: 150, align: 'center' }
   ];
 
   useEffect(() => {
@@ -33,7 +33,6 @@ function Dashboard() {
   const atualizaMural = async () => {
     try {
       const data = await getNaoVisualizados(setMuralData, setLoading, setValid);
-      console.log(data)
       setOpenPopups(new Array(data.length).fill(true));
     } catch (error) {
       console.error('Erro ao exibir os comunicados: ', error);
@@ -41,10 +40,7 @@ function Dashboard() {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      await atualizaMural(setMuralData, setLoading, setValid);
-    };
-    fetchData();
+    atualizaMural();
   }, []);
 
   const handleClosePopup = (index) => {
@@ -59,65 +55,48 @@ function Dashboard() {
         <DadosDash agendamento={agendamentoData} />
         <Box
           gap={1}
-          marginX={1}
           margin={2}
           padding={2}
           width={"868px"}
-          paddingX={2}
-          paddingBottom={3}
-          display="flex"
-          backgroundColor="#FAFAFA"
-          flexDirection="column"
-          style={{ borderRadius: '10px' }}
           component={Paper}
+          style={{ borderRadius: '10px', position: 'relative', zIndex: '0' }}
         >
-          <Box display="flex" fontSize={'24px'} gap={2} marginTop={2} flexDirection={"column"}>
-            <Typography variant="h5" component="h1" style={{ fontSize: '24px' ,fontWeight: 'bold' }}>Últimos agendamentos</Typography>
-            <Table data={agendamentoData} window={"agendamento"} columns={columns} loading={loading} isValid={isValid} hadleDelete={null} PopupManager={null} updateTable={null}/>
-          </Box>
+          <Typography variant="h5" component="h1" style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '30px' }}>Últimos agendamentos</Typography>
+          <Table data={agendamentoData} window={"agendamento"} columns={columns} loading={loading} isValid={isValid} hadleDelete={null} PopupManager={null} updateTable={null} />
         </Box>
       </div>
       <div>
         <Box
           gap={1}
-          marginX={1}
           margin={2}
           padding={2}
           width={"400px"}
           height={"700px"}
-          paddingX={2}
-          paddingBottom={3}
-          display="flex"
-          backgroundColor="#FAFAFA"
-          flexDirection="column"
-          style={{ borderRadius: '10px' }}
           component={Paper}
+          style={{ borderRadius: '10px', position: 'relative', zIndex: '0' }}
         >
-          <Box display="flex" fontSize={'24px'} gap={2} marginTop={2} flexDirection={"column"}>
-            <Typography variant="h5" component="h1" style={{ fontSize: '24px' ,fontWeight: 'bold' }}>Últimos Acessos</Typography>
-          </Box>
+          <Typography variant="h5" component="h1" style={{ fontSize: '24px', fontWeight: 'bold' }}>Últimos Acessos</Typography>
         </Box>
       </div>
-      {muralData.map((item, index) => {
-          return (
-              <div key={index}>
-                  <Popup 
-                      open={openPopups[index]}
-                      handleClose={() => handleClosePopup(index)}
-                      atualizaMural={atualizaMural}
-                      codigoComunicado={item.idComunicado}
-                      title={item.tituloPopup} 
-                      sub={item.titulo} 
-                      description={item.mensagem}
-                      type={item.tipoExibicao} 
-                      enquetes={item.opcaoEnquete}
-                      className="popup"
-                  />
-              </div>
-          );
-      })}
+      {muralData.map((item, index) => (
+        <div key={index} style={{ position: 'relative', zIndex: '2' }}>
+          <Popup
+            open={openPopups[index]}
+            handleClose={() => handleClosePopup(index)}
+            atualizaMural={atualizaMural}
+            codigoComunicado={item.idComunicado}
+            title={item.tituloPopup}
+            sub={item.titulo}
+            description={item.mensagem}
+            type={item.tipoExibicao}
+            enquetes={item.opcaoEnquete}
+            className="popup"
+          />
+        </div>
+      ))}
     </div>
   );
+  
 }
 
 export default Dashboard;
--- site/src/components/Mural/Modal/ExibiComunicado.jsx
+++ site/src/components/Mural/Modal/ExibiComunicado.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { StyledTextField, StyledPaper, FormContainer, Column, FormRow  } from '../../../Utils/StyledForm';
+import { StyledTextField, StyledPaper, FormContainer, Column, FormRow } from '../../../Utils/StyledForm';
 import { validateForm } from '../../Formulario/validation';
-import { Modal, Box, Typography, Link, IconButton } from '@mui/material';
+import { Modal, Box, Typography, Link, IconButton, Paper, styled } from '@mui/material';
 import { showSuccessToast, showErrorToast } from '../../../Utils/Notification';
 import { getAnexo } from '../../../../service/muralService';
 import useForm from '../../Formulario/useForm';
 import { XCircle } from 'phosphor-react';
 import InputMask from 'react-input-mask';
+import Carousel from '../../Carousel';
 
 const ExibiComunicado = ({ codigoComunicado, sub, description, onDataChange, onFieldValidationChange, formData }) => {
-  const [attachment, setAttachment] = useState(null);
+  const [attachment, setAttachment] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const {
     values,
@@ -26,8 +27,6 @@ const ExibiComunicado = ({ codigoComunicado, sub, description, onDataChange, onF
   const handleAttachmentClick = async () => {
     try {
       const anexo = await getAnexo(codigoComunicado);
-      console.log(anexo);
-
       if (anexo) {
         setAttachment(anexo);
         setOpenModal(true);
@@ -44,30 +43,26 @@ const ExibiComunicado = ({ codigoComunicado, sub, description, onDataChange, onF
   };
 
   return (
-    <StyledPaper sx={{background:'#FAFAFA' }} elevation={1} >
-        <Typography variant="h6" component="h7" style={{ fontWeight: 600, fontSize: 24 }}>
-            {sub}
+    <StyledPaper sx={{ background: '#FAFAFA' }} elevation={1}>
+      <Typography variant="h6" component="h7" style={{ fontWeight: 600, fontSize: 24 }}>
+        {sub}
+      </Typography>
+      <StyledPaper sx={{ marginTop: 1, background: '#F5F5F5', border: '1px solid #ABACB2', padding: '10px', textAlign: 'start' }}>
+        <Typography variant="h3" style={{ marginBottom: 2, fontSize: 16 }}>
+          {description}
         </Typography>
-        <StyledPaper sx={{marginTop: 1, background:'#F5F5F5', border: '1px solid #ABACB2', padding:'10px', textAlign:'start'}}>
-            <Typography variant="h3" style={{ marginBottom: 2, fontSize: 16 }}>
-                {description}
-            </Typography>
-        </StyledPaper>
-        <Link variant="h6" component="h7" style={{ color: '#0000EE', fontWeight: 600, float: 'left', lineHeight: '2.6', textDecoration: 'none', cursor: 'pointer', fontSize: 16, textAlign: 'start' }} onClick={handleAttachmentClick}>
-          {"Clica aqui para ver o anexo"}
-        </Link>
-        <Modal open={openModal} onClose={handleCloseModal}>
-          <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '35%', bgcolor: 'background.paper', boxShadow: 24, p: 5, borderRadius: '20px' }}>
-            <IconButton onClick={handleCloseModal} style={{ position: 'absolute', top: 0, right: 0 }}>
-              <XCircle size={28} color="#FF0B0B" />
-            </IconButton>
-            {attachment && attachment.contentType && attachment.contentType.startsWith('image/') ? (
-              <img src={`data:${attachment.contentType};base64,${attachment.data}`} alt="Anexo" style={{ maxWidth: '100%', maxHeight: '100%' }} />
-            ) : (
-              attachment && attachment.contentType && <embed src={`data:${attachment.contentType};base64,${attachment.data}`} type={attachment.contentType} width="100%" height="600px" />
-            )}
-          </Box>
-        </Modal>
+      </StyledPaper>
+      <Link variant="h6" component="h7" style={{ color: '#0000EE', fontWeight: 600, float: 'left', lineHeight: '2.6', textDecoration: 'none', cursor: 'pointer', fontSize: 16, textAlign: 'start' }} onClick={handleAttachmentClick}>
+        {"Clica aqui para ver o anexo"}
+      </Link>
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '40%', height: '80%', bgcolor: 'background.paper', boxShadow: 24, p: 1, borderRadius: '20px', overflow: 'auto' }}>
+          <IconButton onClick={handleCloseModal} style={{ position: 'absolute', top: 0, right: 0 }}>
+            <XCircle size={28} color="#FF0B0B" />
+          </IconButton>
+          <Carousel attachment={attachment} />
+        </Box>
+      </Modal>
     </StyledPaper>
   );
 };
